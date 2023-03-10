9d7337b92a85e0fc2dad41711ef66ed5ec4115dc
Ayush Basu <ayushb3@illinois.edu>
2023-03-09 18:43:16 -0600

    Adding functionality to delete tweets using API (#7)
    Imports are now fixed

--- src/tweeter.py
+++ src/tweeter.py
@@ -1,4 +1,5 @@
 import tweepy
+import datetime
 from settings import API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 from summarizer import summarize
 import os
@@ -16,7 +17,33 @@ def tweet(api: tweepy.API, message: str, date: str):
         tweet_time = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
         api.update_status(message, scheduled_at=tweet_time)
         print('Tweeted successfully!')
-    else if message:
+    elif message:
         api.update_status(message)
     else:
         print('Tweeted unsuccessfully!')
+
+# Function to delete a user selected tweet out of a range of the numTweets most recent tweets
+def delete_tweets(api: tweepy.API, numTweets: int):
+    tweets = api.user_timeline(count=10)
+
+    print("Recent tweets:")
+    for i, tweet in enumerate(tweets):
+        print(f"{i+1} : {tweet.text}")
+    for x in range(3):
+        choice = int(input("Enter the number corresponding to the tweet you want to delete: "))
+
+        try:
+            if choice < 1 or choice > len(tweets):
+                raise ValueError
+            break
+        except ValueError:
+            print("Invalid choice. Please enter a number between 1 and", len(tweets))
+    else:
+        print("You have exceeded the maximum number of attempts. Please try again later.")
+        exit()
+
+
+    tweet_id = tweets[choice-1].id_str
+
+    api.destroy_status(tweet_id)
+    print(f"The tweet has been deleted.")
