6c6d58f3c2c4ac2b9ac6e3b79831507115d943c6
Javier Nieto <javiermnieto89@gmail.com>
2023-03-23 14:26:04 -0500

    print summary during testing

--- tests/test_summarizer.py
+++ tests/test_summarizer.py
@@ -7,4 +7,5 @@ def test_summarizer():
                 Guido van Rossum began working on Python in the late 1980s as a successor to the ABC programming language and first released it in 1991 as Python 0.9.0. Python 2.0 was released in 2000. Python 3.0, released in 2008, was a major revision not completely backward-compatible with earlier versions. Python 2.7.18, released in 2020, was the last release of Python 2.
                 Python consistently ranks as one of the most popular programming languages."""
     have = summarize(text)
+    print(have)
     assert 0 < len(have) < len(text)
