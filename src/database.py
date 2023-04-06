1394ef52fc20187e1c325c7927e8da762b4edcd7
Milnil <90474265+Milnil@users.noreply.github.com>
2023-04-06 17:44:10 -0500

    Modified database structure to add ids and changed some functionalities. (#10)
    * Created function to setup database, insert, and retrieve along with a small test to ensure functionality
    
    * added some more functionality to database stuff

--- src/database.py
+++ src/database.py
@@ -9,7 +9,7 @@ def create_table():
 
     # Create the articles table if it does not exist
     c.execute('''CREATE TABLE IF NOT EXISTS articles
-                 (title TEXT, author TEXT, date TEXT, content TEXT)''')
+                 (article_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, author TEXT, date TEXT, content TEXT, hasTweeted BOOLEAN)''')
 
     # Commit the changes and close the connection
     conn.commit()
@@ -24,23 +24,42 @@ def insert_article(title, author, date, content):
     c = conn.cursor()
 
     # Insert a new row into the articles table
-    c.execute("INSERT INTO articles (title, author, date, content) VALUES (?, ?, ?, ?)",
-              (title, author, date, content))
+    c.execute("INSERT INTO articles (title, author, date, content, hasTweeted) VALUES (?, ?, ?, ?, ?)",
+              (title, author, date, content, False))
 
     # Commit the changes and close the connection
     conn.commit()
     conn.close()
 
-# Function that can be used to acquire content from the 
-# database given certain information about an article
-def get_article_content(title, author, date):
+
+
+# Gets all the information stored about an article
+def get_single_article(article_id):
     # Connect to the database
     conn = sqlite3.connect('articles.db')
     c = conn.cursor()
 
     # Execute a SELECT query to retrieve the content of the article
-    c.execute("SELECT content FROM articles WHERE title = ? AND author = ? AND date = ?",
-              (title, author, date))
+    c.execute("SELECT * FROM articles WHERE article_id = ?",
+              (article_id,))
+    result = c.fetchone()
+
+    # Close the connection
+    conn.close()
+
+    # Return the content of the article 
+    return result
+
+# Function that can be used to acquire content from the 
+# database given certain information about an article
+def get_single_article_content(article_id):
+    # Connect to the database
+    conn = sqlite3.connect('articles.db')
+    c = conn.cursor()
+
+    # Execute a SELECT query to retrieve the content of the article
+    c.execute("SELECT content FROM articles WHERE article_id = ?",
+              (article_id,))
     result = c.fetchone()
 
     # Close the connection
@@ -49,21 +68,34 @@ def get_article_content(title, author, date):
     # Return the content of the article 
     return result[0]
 
-# Gets all the information stored about an article
-def get_article(title, author, date):
+def get_all_new_articles(hasDate: bool):
     # Connect to the database
     conn = sqlite3.connect('articles.db')
     c = conn.cursor()
 
-    # Execute a SELECT query to retrieve the content of the article
-    c.execute("SELECT * FROM articles WHERE title = ? AND author = ? AND date = ?",
-              (title, author, date))
-    result = c.fetchone()
+    if hasDate:
+        c.execute("SELECT article_id, content, date FROM articles WHERE date IS NOT NULL AND hasTweeted = FALSE")
+    else:
+        c.execute("SELECT article_id, content FROM articles WHERE date IS NULL AND hasTweeted = FALSE")
+    results = c.fetchall()
 
     # Close the connection
     conn.close()
 
-    # Return the content of the article 
+    # Return the content of the articles 
+    return results
+
+
+
+def delete_article(article_id):
+    conn = sqlite3.connect('articles.db')
+    c = conn.cursor()
+
+    c.execute("DELETE FROM articles WHERE article_id=?", (article_id,))
+
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
     return result
 
 def pick_article():
