bf2940c5d406390701a44e20f0d8087ef02be26e
Javier Nieto <javiermnieto89@gmail.com>
2023-04-19 18:14:34 -0500

    tweeter class init (#23)

--- src/tweeter.py
+++ src/tweeter.py
@@ -1,103 +1,22 @@
 import tweepy
-import schedule
-import time
-import datetime
-from settings import API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
-from src.summarizer import summarize
-import os
-from src.database import get_all_new_articles, delete_article
 
 
-def api():
-    auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+class Tweeter:
+    def __init__(self, api_key, api_secret, acces_token, access_token_secret) -> None:
+        auth = tweepy.OAuthHandler(api_key, api_secret)
+        auth.set_access_token(acces_token, access_token_secret)
+        self.api = tweepy.API(auth)
 
-    return tweepy.API(auth)
+    @property
+    def tweets(self):
+        for t in tweepy.Cursor(self.api.user_timeline).items():
+            yield t
 
+    def tweet(self, message: str) -> tweepy.Models.Status:
+        return self.api.update_status(message)
 
-def tweeter():
-    # TO DO: not necessary to have hasDate anymore if we are deleting after tweeting
-    tweets_to_be_scheduled = get_all_new_articles(hasDate=True)
-    tweets_to_be_put_on_scheduler = get_all_new_articles(hasDate=False)
-    for t in tweets_to_be_scheduled:
-        tweet(api=api(), message=t[1], date=t[2])
-        delete_article(t[0])
-    schedule.every().day.at("12:00").do(
-        scheduled_tweeter, tweets_to_be_put_on_scheduler)
+    def delete_tweet(self, tweet_id: int):
+        self.api.destroy_status(tweet_id)
 
-    # Schedule the deletion of tweets that are in the database longer than 2 weeks
-    schedule.every().day.at("12:00").do(database.delete_old_rows)
-
-    # NOTE: check if parameters are passed as references so that we move onto different tweets
-    # Should be fine because argument is a mutable list
-
-    # if we run out of tweets, stop the schedule
-    while len(schedule.jobs) != 0:
-        schedule.run_pending()
-        time.sleep(1)
-
-# Function to publish tweet with optional date for scheduling for an article that has not been released yet
-# def tweet(api: tweepy.API, message: str, date: str):
-#     if message:
-#         if date:
-#             tweet_time = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-#             api.update_status(message, scheduled_at=tweet_time)
-#             print('Tweeted successfully!')
-#         api.update_status(message)
-#     else:
-#         print('Tweeted unsuccessfully!')
-
-# Function to schedule tweets for a specified frequency given in hours
-
-
-def scheduled_tweeter(tweets):
-    item = tweets.pop(0)
-    tweet(api=api(), message=item[1], date=None)
-    delete_article(item[0])
-
-
-# Function to delete a user selected tweet out of a range of the numTweets most recent tweets
-# def delete_tweets(api: tweepy.API, numTweets: int):
-#     tweets = api.user_timeline(count=10)
-
-#     print("Recent tweets:")
-#     for i, tweet in enumerate(tweets):
-#         print(f"{i+1} : {tweet.text}")
-#     for x in range(3):
-#         choice = int(input("Enter the number corresponding to the tweet you want to delete: "))
-
-#         try:
-#             if choice < 1 or choice > len(tweets):
-#                 raise ValueError
-#             break
-#         except ValueError:
-#             print("Invalid choice. Please enter a number between 1 and", len(tweets))
-#     else:
-#         print("You have exceeded the maximum number of attempts. Please try again later.")
-#         exit()
-
-
-#     tweet_id = tweets[choice-1].id_str
-
-#     api.destroy_status(tweet_id)
-#     print(f"The tweet has been deleted.")
-
-def tweets(api: tweepy.API):
-    for t in tweepy.Cursor(api.user_timeline).items():
-        yield t
-
-
-def tweet(api: tweepy.API, message: str, date: str = None):
-    """Function to publish tweet with optional date for scheduling"""
-
-    if message and date:
-        tweet_time = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-        api.update_status(message, scheduled_at=tweet_time)
-    elif message:
-        api.update_status(message)
-
-
-def delete_tweets(api: tweepy.API, statuses: list):
-    """Function to delete a tweets from given list of tweet statuses"""
-    for status in statuses:
-        api.destroy_status(status.id)
+    def update_tweet(self, tweet_id: int, message: str):
+        self.api.update_status(message, tweet_id)
